8d35bc322bd515fb0644aa797dcdee8d00176946
luizzmg <129332942+luizzmg@users.noreply.github.com>
2023-09-22 11:23:19 -0300

    otimizando o arquivo de coletáveis
    Signed-off-by: luizzmg <129332942+luizzmg@users.noreply.github.com>

--- main project/collectibles.py
+++ main project/collectibles.py
@@ -1,6 +1,5 @@
 import pygame
-from random import randint
-from level import levels_size
+import random
 
 red = (255, 0, 0)
 green = (0, 255, 0)
@@ -8,8 +7,31 @@ blue = (0, 0, 255)
 yellow = (255,255,0)
 
 class Collectible:
-    def __init__(self, place, color, colle_x, colle_y,id):
-        self.rect = pygame.draw.rect(place,color,(colle_x,colle_y,20,20))
-        self.color = color
-        self.id = id
-        self.collected = False
+    def __init__(self, id):
+        self.rect = pygame.Rect(0,0,20,20) # um quadrado dele
+        self.id = id                       # pode ser r g b y
+        self.collected = False             # se foi coletado
+        self.pos_x = int()                 # o indice dele na linha da matriz
+        self.pos_y = int()                 # o indice dele na coluna da matriz
+        self.color = tuple()               # a cor dele mesmo
+
+        match self.id:
+            case 'r': # quadrante superior esquerdo
+                self.pos_x = random.choice(range(0,6))
+                self.pos_y = random.choice(range(0,6))
+                self.color = red
+            
+            case 'g': # quadrante superior direito
+                self.pos_x = random.choice(range(7,14))
+                self.pos_y = random.choice(range(0,6))
+                self.color = green
+            
+            case 'b': # quadrante inferior esquerdo
+                self.pos_x = random.choice(range(0,6))
+                self.pos_y = random.choice(range(7,14))
+                self.color = blue
+
+            case 'y': # quadrante inferior direito
+                self.pos_x = random.choice(range(7,14))
+                self.pos_y = random.choice(range(7,14))
+                self.color = yellow
