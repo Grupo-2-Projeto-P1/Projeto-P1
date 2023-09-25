c6cfa39432fdf45751aab702f7d9d6f88df392ae
Vitor Mendonça <83474640+VitorMendonca62@users.noreply.github.com>
2023-09-25 18:21:54 -0300

    otimizando mais uma vez o codigo de luiz
    Signed-off-by: Vitor Mendonça <83474640+VitorMendonca62@users.noreply.github.com>

--- main project/collectibles.py
+++ main project/collectibles.py
@@ -1,37 +1,27 @@
 import pygame
-import random
+from random import choice
+from colors import *
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255,255,0)
+color_k = {'r': red, 'g': green, 'b': blue, 'y': yellow}
 
 class Collectible:
     def __init__(self, id):
         self.rect = pygame.Rect(0,0,20,20) # um quadrado dele
         self.id = id                       # pode ser r g b y
         self.collected = False             # se foi coletado
-        self.pos_x = int()                 # o indice dele na linha da matriz
-        self.pos_y = int()                 # o indice dele na coluna da matriz
-        self.color = tuple()               # a cor dele mesmo
+        self.coords = list()
+        self.color = color_k[id]           # a cor dele mesmo
+        self.collected_string = "Não coletado"
 
-        match self.id:
-            case 'r': # quadrante superior esquerdo
-                self.pos_x = random.choice(range(0,6))
-                self.pos_y = random.choice(range(0,6))
-                self.color = red
-            
-            case 'g': # quadrante superior direito
-                self.pos_x = random.choice(range(7,14))
-                self.pos_y = random.choice(range(0,6))
-                self.color = green
-            
-            case 'b': # quadrante inferior esquerdo
-                self.pos_x = random.choice(range(0,6))
-                self.pos_y = random.choice(range(7,14))
-                self.color = blue
+        self.take_interval(self.id)
+        self.pos_x = self.coords[0]                 # o indice dele na linha da matriz
+        self.pos_y = self.coords[1]                 # o indice dele na coluna da matriz
+    
+    def take_interval(self, id):
+        range0_6 = range(0,6)
+        range7_14 = range(7,14)
 
-            case 'y': # quadrante inferior direito
-                self.pos_x = random.choice(range(7,14))
-                self.pos_y = random.choice(range(7,14))
-                self.color = yellow
+        if id == "r": self.coords = [choice(range0_6), choice(range0_6)]
+        if id == "g": self.coords = [choice(range7_14), choice(range0_6)]
+        if id == "b": self.coords = [choice(range0_6), choice(range7_14)]
+        if id == "y": self.coords = [choice(range7_14), choice(range7_14)]
